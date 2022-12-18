349abc1e0fdf8935931f7ea470a4f22ba3dc9354
zyh <zzzzzyh111@163.com>
2022-12-18 13:32:37 +0800

    lab double update

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import numpy as np
 
 m = 100
 path = '/home/zyh/catkin_ws/src/UAV/scripts/Record/'
-ep_reward_arr_1 = np.loadtxt(path + 'DQN_Reward.txt')
+ep_reward_arr_1 = np.loadtxt(path + 'DQN_Reward_lab1_Random.txt')
 ep_reward_arr_2 = np.loadtxt(path + 'DDQN_Reward_lab1.txt')
 n = len(ep_reward_arr_1) // m
 avg_reward_arr_1 = np.mean(np.reshape(ep_reward_arr_1[:m*n], [n, m]), 1)
