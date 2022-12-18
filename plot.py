734c69585b0997a2d5ead64a21eb84c033ace909
zzzzzyh111 <zzzzzyh111@163.com>
2022-12-18 17:49:22 +0800

    plot

--- plot.py
+++ plot.py
@@ -1,20 +1,21 @@
 #!/home/yuhang/anaconda3/bin/python3
 # -*- coding: utf-8 -*-
-
 import matplotlib.pyplot as plt
 import numpy as np
-
 m = 100
-path = '/home/zyh/catkin_ws/src/UAV/scripts/Record/'
+path = '/home/yuhang/catkin_ws/src/uav_ros/scripts/Record/'
 ep_reward_arr_1 = np.loadtxt(path + 'DQN_Reward.txt')
 ep_reward_arr_2 = np.loadtxt(path + 'DDQN_Reward_lab1.txt')
+ep_reward_arr_3 = np.loadtxt(path + 'Duel_DQN_Reward_home1.txt')
 n = len(ep_reward_arr_1) // m
 avg_reward_arr_1 = np.mean(np.reshape(ep_reward_arr_1[:m*n], [n, m]), 1)
 n = len(ep_reward_arr_2) // m
 avg_reward_arr_2 = np.mean(np.reshape(ep_reward_arr_2[:m*n], [n, m]), 1)
-
+n = len(ep_reward_arr_3) // m
+avg_reward_arr_3 = np.mean(np.reshape(ep_reward_arr_3[:m*n], [n, m]), 1)
 plt.plot(avg_reward_arr_1, label='DQN')
-plt.plot(avg_reward_arr_2, label='DDQN')
+plt.plot(avg_reward_arr_2, label='Doouble-DQN')
+plt.plot(avg_reward_arr_3, label='Duel-DQN')
 plt.xlabel('Episode')
 plt.ylabel('Reward')
 plt.legend(loc='best')
