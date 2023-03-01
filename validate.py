0cccae9fe6c1dacb3f9c4583967d636ff5106339
zzzzzyh111 <zzzzzyh111@163.com>
2023-03-01 22:12:14 +0800

    validate

--- validate.py
+++ validate.py
@@ -9,9 +9,9 @@ import torch
 
 GazeboUAV = env.GazeboUAV()
 agent = ddqn.DQN(GazeboUAV, batch_size=64, memory_size=10000, target_update=4,
-                gamma=0.99, learning_rate=1e-4, eps_min=0, eps_period=5000)
-# param_path = '/home/yuhang/catkin_ws/src/uav_ros/scripts/Record/'
-param_path = '/home/zyh/catkin_ws/src/UAV/scripts/Record/dqn_1st.pth'
+                gamma=0.99, learning_rate=1e-4, eps=0, eps_min=0, eps_period=5000)
+param_path = '/home/yuhang/catkin_ws/src/uav_ros/scripts/Record/Duel_DQN_Reward_home2_sup.pth'
+# param_path = '/home/zyh/catkin_ws/src/UAV/scripts/Record/dqn_1st.pth'
 agent.load_model(param_path, map_location=torch.device('cpu'))
 while True:
     state1, state2 = GazeboUAV.reset()
